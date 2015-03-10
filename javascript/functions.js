eed55265023557c5d658d0ab0122396a6010e80c
Chris Turner <chris.turner@minedu.govt.nz>
2015-03-11 11:10:11 +1300

    Initial commit

---
+++ javascript/functions.js
@@ -0,0 +1,49 @@
+(function ($) {
+})(window.jQuery);
+
+jQuery(function ($) {
+    "use strict";
+
+    var rootUrl = $("#calendar").data("root-url");
+
+    /**
+     * Makes ajax call to gather events
+     */
+    function loadCalendar() {
+        $.ajax({
+            url: rootUrl + "/eventsAsJson",
+            type: "GET",
+            cache: true,
+            success: function (json) {
+                $('#calendar').fullCalendar({
+                    events: json,
+                    firstDay: 1,
+                    columnFormat: 'dddd'
+                })
+            }
+        });
+    }
+
+    /**
+     * Tidies up the ajax function, removes the loader when ready.
+     *
+     * Ensures the calendar is hidden while loading regardless of CSS properties.
+     */
+    $(document).on({
+        ajaxStart: function () {
+            $('#calendar').css({'visibility': 'hidden', 'opacity': 0});
+            $('.document-overlay').css({'visibility': 'visible', 'opacity': 1});
+        },
+        ajaxStop: function () {
+            $('.document-overlay').css({'visibility': 'hidden', 'opacity': 0, 'display': 'none'});
+            $('#calendar').css({'visibility': 'visible', 'opacity': 1});
+        }
+    });
+
+    /**
+     * Called when the page is ready
+     */
+    $(document).ready(function () {
+        loadCalendar();
+    });
+});
