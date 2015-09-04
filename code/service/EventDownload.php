925abc0550aac304e33ac5471c389f484b3ec83a
Chris Turner <chris.turner@education.govt.nz>
2015-09-04 16:42:36 +1200

    Rework .ics save function as service - W.I.P

--- code/service/EventDownload.php
+++ code/service/EventDownload.php
@@ -1,201 +1,100 @@
 <?php
 
 /**
- * Created by PhpStorm.
- * User: lucy
- * Date: 4/09/15
- * Time: 10:45 AM
+ * Class EventDownload
  */
-class EventDownload
-{
+class EventDownload {
 
 	/**
-	 * Gets all possible events from database
+	 * @var
+	 */
+	private $file;
+
+	/**
+	 * @var
+	 */
+	private $filePath;
+
+	/**
+	 *
+	 */
+	public function __construct() {
+
+		$this->setupFile();
+	}
+
+	/**
+	 * @param $timestamp
+	 *
+	 * @return bool|string
+	 */
+	function dateToCal($timestamp) {
+
+		return date('Ymd\THis\Z', $timestamp);
+	}
+
+	/**
+	 * @param $string
 	 *
 	 * @return mixed
 	 */
-	public function getEvents()
-	{
-		return FullCalendarEvent::get();
-	}
-	//
-	/**
-	 *
-	 * Call this function to download the invite.
-	 */
-	/**
-	 *
-	 * Downloaded constant
-	 *
-	 * @var cost
-	 *
-	 */
-	const DOWNLOADED = 100;
-	public function download()
-	{
-		$_SESSION['calander_invite_downloaded'] = self::DOWNLOADED;
-		$generate = $this->_generate();
+	function escapeString($string) {
 
+		$string = strip_tags($string);
+		$string = substr($string, 0, 100);
 
-
-		header("Pragma: public");
-		header("Expires: 0");
-		header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
-		header("Cache-Control: public");
-		header("Content-Description: File Transfer");
-		header("Content-type: application/octet-stream");
-		header("Content-Disposition: attachment; filename=\"invite.ics\"");
-		header("Content-Transfer-Encoding: binary");
-		header("Content-Length: " . strlen($generate));
-		print $generate;
+		return preg_replace('/([\,;])/', '\\\$1', $string);
 	}
 
 	/**
 	 *
-	 * Save the invite to a file
-	 *
-	 * @param string $path
-	 * @param string $name
-	 * @return \Invite
-	 *
 	 */
-	public function save($path = null, $name = null)
-	{
-		if (null === $path) {
-			$path = $this->_savePath;
+	public function setupFile() {
+
+		$folder = Folder::find_or_make('/CalendarFiles/');
+		$this->filePath = 'assets/CalendarFiles/' . date("H:i:s") . 'test.ics';
+
+		$this->file = new File();
+		$this->file->Name = $this->filePath;
+		$this->file->Filename = $this->filePath;
+		$this->file->ParentID = $folder->ID;
+
+		$this->file->write();
+
+		file_put_contents('../'
+			. $this->filePath, "BEGIN:VCALENDAR\n", FILE_APPEND);
+		file_put_contents('../'
+			. $this->filePath, "VERSION:2.0\n", FILE_APPEND);
+	}
+
+	/**
+	 * @param $ID
+	 */
+	public function generateEventList($ID) {
+
+		$events = FullCalendarEvent::get()->filter(array("ParentID" => $ID));
+
+		$content = null;
+		foreach ($events as $event) {
+			$start = $this->dateToCal(strtotime($event->StartDate . "H:i:s"));
+			$end = $this->dateToCal(strtotime($event->EndDate . "H:i:s"));
+			$description = $this->escapeString($event->ShortDescription);
+
+			$content .= "BEGIN:VEVENT\n";
+			$content .= "CLASS:PUBLIC\n";
+			$content .= "DESCRIPTION:{$description}\n";
+			$content .= "DTSTART:{$start}\n";
+			$content .= "DTEND:{$end}\n";
+			$content .= "LOCATION:(null)\n";
+			$content .= "TRANSP:TRANSPARENT\n";
+			$content .= "END:VEVENT\n";
+
+			file_put_contents('../' . $this->filePath, $content, FILE_APPEND);
 		}
 
-		if (null === $name) {
-			$name = $this->getUID() . '.ics';
-		}
-
-		// create path if it doesn't exist
-		if (!is_dir($path)) {
-			try {
-				mkdir($path, 0777, TRUE);
-			} catch (Exception $e) {
-				die('Unabled to create save path.');
-			}
-		}
-
-		if (($data = $this->getInviteContent()) == TRUE) {
-			try {
-				$handler = fopen($path . $name, 'w+');
-				$f = fwrite($handler, $data);
-				fclose($handler);
-
-				// saving the save name
-				$_SESSION['savepath'] = $path . $name;
-			} catch (Exception $e) {
-				die('Unabled to write invite to file.');
-			}
-		}
-
-		return $this;
-	}
-
-	/**
-	 * Get the saved invite path
-	 * @return string|boolean
-	 */
-	public static function getSavedPath()
-	{
-		if (isset($_SESSION['savepath'])) {
-			return $_SESSION['savepath'];
-		}
-
-		return false;
-	}
-
-	/**
-	 *
-	 * Check to see if the invite has been downloaded or not
-	 *
-	 * @return boolean
-	 *
-	 */
-	public static function isDownloaded()
-	{
-		if ($_SESSION['calander_invite_downloaded'] == self::DOWNLOADED) {
-			return true;
-		}
-
-		return false;
-	}
-
-
-
-	/**
-	 *
-	 * Get the content of for and invite. Returns false if the invite
-	 * was unable to be generated.
-	 * @return string|boolean
-	 */
-	public function getInviteContent()
-	{
-		if (!$this->_generated) {
-
-				if ($this->_generate()) {
-					return $this->_generated;
-				}
-
-
-		}
-
-		return $this->_generated;
-	}
-
-	/**
-	 *
-	 * Generate the content for the invite.
-	 *
-	 * @return \Invite
-	 *
-	 */
-	public function generate()
-	{
-		$this->_generate();
-		return $this;
-	}
-
-	/**
-	 *
-	 * The function generates the actual content of the ICS
-	 * file and returns it.
-	 *
-	 * @return string|bool
-	 */
-	private function _generate()
-	{
-
-
-			$content = "";
-			foreach(FullCalendarEvent::get() as $event) {
-
-				$content = "BEGIN:VCALENDAR\n";
-				$content .= "VERSION:2.0\n";
-				$content .= "CALSCALE:GREGORIAN\n";
-				$content .= "METHOD:REQUEST\n";
-				$content .= "BEGIN:VEVENT\n";
-				$content .= "DTSTART:{$event->StartDate}\n";
-				$content .= "DTEND:{$event->EndDate}\n";
-				$content .= "DESCRIPTION:{$event->ShortDescription}\n";
-				$content .= "SEQUENCE:0\n";
-				$content .= "STATUS:NEEDS-ACTION\n";
-				$content .= "TRANSP:OPAQUE\n";
-				$content .= "END:VEVENT\n";
-				$content .= "END:VCALENDAR";
-
-			}
-
-		var_dump($content);
-		die;
-
-			return $content;
-
-
+		file_put_contents('../'
+			. $this->filePath, "END:VCALENDAR\n", FILE_APPEND);
 
 	}
-
 
 }
